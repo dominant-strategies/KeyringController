6da18b36a597c0504e2f84a4e266fa5466ef8798
Alan Orwick <alan.kev11@gmail.com>
2021-11-16 13:07:17 -0600

    Update import for keyring

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const encryptor = require('browser-passworder');
 const { normalize: normalizeAddress } = require('eth-sig-util');
 
 const SimpleKeyring = require('eth-simple-keyring');
-const HdKeyring = require('quai-hd-keyring');
+const HdKeyring = require('@metamask/quai-hd-keyring');
 
 const keyringTypes = [SimpleKeyring, HdKeyring];
 
